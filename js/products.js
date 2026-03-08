56de29efcb57e3a02bd83030585d496dcedf5dfa
treza8347-a11y <treza8347@gmail.com>
2026-03-08 16:57:33 +0100

    Add initial products data to products.js

---
+++ js/products.js
@@ -0,0 +1,27 @@
+const products = [
+
+{
+id:1,
+name:"Casque Audio Premium",
+price:89,
+image:"https://picsum.photos/400?1",
+description:"Casque audio haute qualité avec réduction de bruit."
+},
+
+{
+id:2,
+name:"Montre Connectée",
+price:129,
+image:"https://picsum.photos/400?2",
+description:"Montre intelligente pour suivre votre activité."
+},
+
+{
+id:3,
+name:"Chaussures Sport",
+price:79,
+image:"https://picsum.photos/400?3",
+description:"Chaussures confortables pour le sport."
+}
+
+];
