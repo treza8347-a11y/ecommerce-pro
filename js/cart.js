12d7d29032061fb32c7f076515240654fd5dd40c
treza8347-a11y <treza8347@gmail.com>
2026-03-08 17:00:38 +0100

    Implement addToCart function to manage cart

---
+++ js/cart.js
@@ -0,0 +1,13 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(id){
+
+const product = products.find(p => p.id === id);
+
+cart.push(product);
+
+localStorage.setItem("cart", JSON.stringify(cart));
+
+alert("Produit ajouté au panier");
+
+}
